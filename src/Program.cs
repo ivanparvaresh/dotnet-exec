f09bfbc0122d1b3443c92016186895dd322e4c5e
Ivan <javadparvaresh@gmail.com>
2019-12-19 18:07:10 +0300

    Fix exception handling

--- src/Program.cs
+++ src/Program.cs
@@ -1,19 +1,28 @@
-﻿namespace Ivanize.DotnetTool.Exec
+﻿using System;
+
+namespace Ivanize.DotnetTool.Exec
 {
     class Program
     {
         const string FILENAME = "./.dotnetexec.json";
         static void Main(string[] args)
         {
-            var defaultEntrypointDetector = new DefaultEntrypointDetector();
-            var parser = new ScriptsFileParser(defaultEntrypointDetector);
-            if (!System.IO.File.Exists(FILENAME))
-                throw new System.IO.FileNotFoundException();
+            try
+            {
+                var defaultEntrypointDetector = new DefaultEntrypointDetector();
+                var parser = new ScriptsFileParser(defaultEntrypointDetector);
+                if (!System.IO.File.Exists(FILENAME))
+                    throw new System.IO.FileNotFoundException($"No '{FILENAME}' file found at {System.IO.Directory.GetCurrentDirectory()}");
 
-            var package = parser.Parse(System.IO.File.OpenText(FILENAME));
+                var package = parser.Parse(System.IO.File.OpenText(FILENAME));
 
-            var executor = new Executor(package);
-            executor.Execute(args);
+                var executor = new Executor(package);
+                executor.Execute(args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
